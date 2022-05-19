8544818444a532587a0c67c4bbba3105b0e272bc
Vall-Justice <104562832+Vall-Justice@users.noreply.github.com>
2022-05-19 13:40:51 +0700

    Tugas 9 Javascript

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+//TUGAS 9 JS
+let identitas =  {
+   nama: "Andi",
+   status: "Pelajar",
+   hobi: ["olahraga", "ngoding"],
+   
+};
+console.log(identitas.status);
+
+for(let x in identitas) {
+   console.log(identitas[x]);
+}
+
